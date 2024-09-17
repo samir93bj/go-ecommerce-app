c58b6d98b9d0f809e90c7efd9291a0ff261f3452
Samir Mahmud <samir93bj@gmail.com>
2024-09-16 22:42:01 -0300

    Refactor UserService and add user-related methods

--- internal/service/UserService.go
+++ internal/service/UserService.go
@@ -1,5 +1,90 @@
 package service
 
-type UserService struct {
+import "go-ecommerce-app/internal/domain"
 
+type UserService struct {
+}
+
+func (s *UserService) FindUserByEmail(email string) (*domain.User, error) {
+	// Implement the logic to find a user by email
+
+	return nil, nil
+}
+
+func (s *UserService) SingUp(input any) (string, error) {
+	// Implement the logic to create a user
+
+	return "", nil
+}
+
+func (s *UserService) Login(input any) (string, error) {
+	// Implement the logic to create a user
+
+	return "", nil
+}
+
+func (s *UserService) GetVerificationCode(user domain.User) (int, error) {
+	// Implement the logic to create a user
+
+	return 0, nil
+}
+
+func (s *UserService) VerifyCode(id uint, code int) error {
+	// Implement the logic to create a user
+
+	return nil
+}
+
+func (s *UserService) CreateProfile(id uint, input any) error {
+	// Implement the logic to create a user
+
+	return nil
+}
+
+func (s *UserService) GetProfile(id uint) (*domain.User, error) {
+	// Implement the logic to create a user
+
+	return nil, nil
+}
+
+func (s *UserService) UpdateProfile(id uint, input any) error {
+	// Implement the logic to create a user
+
+	return nil
+}
+
+func (s *UserService) BecomeSeller(id uint, input any) (string, error) {
+	// Implement the logic to create a user
+
+	return "", nil
+}
+
+func (s *UserService) FindCart(id uint) ([]interface{}, error) {
+	// Implement the logic to create a user
+
+	return nil, nil
+}
+
+func (s *UserService) CreateCart(input any, u domain.User) ([]interface{}, error) {
+	// Implement the logic to create a user
+
+	return nil, nil
+}
+
+func (s *UserService) CreateOrder(u domain.User) (int, error) {
+	// Implement the logic to create a user
+
+	return 0, nil
+}
+
+func (s *UserService) GetOrders(U domain.User) ([]interface{}, error) {
+	// Implement the logic to create a user
+
+	return nil, nil
+}
+
+func (s *UserService) GetOrderById(id uint, uId uint) ([]interface{}, error) {
+	// Implement the logic to create a user
+
+	return nil, nil
 }
